ee7dee960cf24aa14d3d252335e12485dea404e0
jing929 <tenshoku0801@gmail.com>
2024-10-04 20:38:03 +0900

    ユーザー管理機能の実装修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :email, uniqueness: true
   validates :password, format: { with: /\A(?=.?[a-z])(?=.?[\d])[a-z\d]+\z/i}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.email}
-    password              {Faker::Internet.password + '1a' (min_length: 6)}
+    password              { "1a1a1a1a" }
     password_confirmation {password}
     first_name            {"太郎"}
     last_name             {"山田"}
