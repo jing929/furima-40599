4087535446b42ac40388c5580f184d08a286d0dc
jing929 <tenshoku0801@gmail.com>
2024-10-04 20:48:30 +0900

    ユーザー管理機能の実装修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, uniqueness: true
   validates :password, format: { with: /\A(?=.?[a-z])(?=.?[\d])[a-z\d]+\z/i}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
   validates :birthday, presence: true
 
   # has_many :items, dependent: :destroy
