fc725aaa7cf37e41d9040a781940e6526b2e9862
jing929 <tenshoku0801@gmail.com>
2024-10-04 19:17:09 +0900

    ユーザー管理機能の実装修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/}
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン一]+\z/}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン一]+\z/}
+  validates :password, format: { with: /\A(?=.?[a-z])(?=.?[\d])[a-z\d]+\z/i}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/}
   validates :birthday, presence: true
 
-  has_many :items, dependent: :destroy
-  has_many :orders, dependent: :destroy
+  # has_many :items, dependent: :destroy
+  # has_many :orders, dependent: :destroy
 end
