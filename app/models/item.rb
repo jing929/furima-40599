fd0e5705140a9a7a29a0ec6d342ab3a293274016
jing929 <tenshoku0801@gmail.com>
2024-10-12 16:21:19 +0900

    商品出品機能修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
-  has_one :order
+  # has_one :order
   
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
@@ -18,7 +18,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "Must be other than 1" }
   validates :shopping_cost_id, presence: true, numericality: { other_than: 1, message: "Must be other than 1" }
   validates :shopping_day_id, presence: true, numericality: { other_than: 1, message: "Must be other than 1" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image, presence: true
 
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,77 +31,63 @@ RSpec.describe Item, type: :model do
       end
 
       it 'category_idが---では登録できない' do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category Must be other than 1")
       end
 
       it 'condition_idが---では登録できない' do
-        @item.condition_id = "1"
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition Must be other than 1")
       end
 
       it 'prefecture_idが---では登録できない' do
-        @item.prefecture_id = "1"
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture Must be other than 1")
       end
 
       it 'shopping_cost_idが---では登録できない' do
-        @item.shopping_cost_id = "1"
+        @item.shopping_cost_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shopping cost Must be other than 1")
       end
 
       it 'shopping_day_idが---では登録できない' do
-        @item.shopping_day_id = "1"
+        @item.shopping_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shopping day Must be other than 1")
       end
 
-      context '価格が保存可能な場合' do
-        it '価格が¥300以上の場合は保存できる' do
-          @item.price = 300
-          expect(@item).to be_valid
-        end
-
-        it '価格が¥9,999,999以下の場合は保存できる' do
-          @item.price = 9_999_999
-          expect(@item).to be_valid
-        end
+      it '価格が¥300未満では保存できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
 
-      context '価格が保存できない場合' do
-        it '価格が¥300未満では保存できない' do
-          @item.price = 299
-          @item.valid?
-          expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
-        end
+      it '価格が¥9,999,999を超える場合は保存できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
 
-        it '価格が¥9,999,999を超える場合は保存できない' do
-          @item.price = 10_000_000
-          @item.valid?
-          expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
-        end
+      it '価格が半角数値でないと保存できない' do
+        @item.price = '３００' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
 
-        it '価格が半角数値でないと保存できない' do
-          @item.price = '３００' 
-          @item.valid?
-          expect(@item.errors.full_messages).to include("Price is not a number")
-        end
+      it '価格が数値以外の文字列では保存できない' do
+        @item.price = 'abc'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
 
-        it '価格が数値以外の文字列では保存できない' do
-          @item.price = 'abc'
-          @item.valid?
-          expect(@item.errors.full_messages).to include("Price is not a number")
-        end
-
-        it '価格が空では保存できない' do
-          @item.price = ""
-          @item.valid?
-          expect(@item.errors.full_messages).to include("Price can't be blank")
-        end
+      it '価格が空では保存できない' do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
     end
   end
