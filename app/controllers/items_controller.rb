753fcd127b3143ee3f65fa50ce75b5b9166985da
jing929 <tenshoku0801@gmail.com>
2024-10-13 16:25:56 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -19,10 +21,29 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update 
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: '商品情報が更新されました'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def set_item
     @item = Item.find(params[:id])
   end
 
-  private
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
 
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :prefecture_id, :shopping_cost_id, :shopping_day_id, :price, :image).merge(user_id: current_user.id)
